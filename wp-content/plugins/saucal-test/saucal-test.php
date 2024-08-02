05fe8e3f5c7f380a4fa934309113a9f1adbb3f5b
tjrodrigues7 <131903785+tjrodrigues7@users.noreply.github.com>
2024-08-03 00:29:01 +0100

    Add a new tab on My account page
    New tab on my account page where the widget and shortcode will be displayed

--- wp-content/plugins/saucal-test/saucal-test.php
+++ wp-content/plugins/saucal-test/saucal-test.php
@@ -1,101 +1,42 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              https://https://www.linkedin.com/in/ivan-rodrigues7/
- * @since             1.0.0
- * @package           Saucal_Test
- *
- * @wordpress-plugin
  * Plugin Name:       Saucal Test
- * Plugin URI:        https://https://www.linkedin.com/in/ivan-rodrigues7/
- * Description:       Just a custom plugin for the Saucal test
+ * Plugin URI:        https://www.linkedin.com/in/ivan-rodrigues7/
+ * Description:       A custom plugin for the Saucal test with OMDB API settings and WooCommerce custom tab.
  * Version:           1.0.0
  * Author:            Ivan Rodrigues
- * Author URI:        https://https://www.linkedin.com/in/ivan-rodrigues7//
+ * Author URI:        https://www.linkedin.com/in/ivan-rodrigues7/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       saucal-test
  * Domain Path:       /languages
  */
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
 }
 
 /**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
+ * Add the OMDB API menu item.
  */
-define( 'SAUCAL_TEST_VERSION', '1.0.0' );
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-saucal-test-activator.php
- */
-function activate_saucal_test() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-saucal-test-activator.php';
-	Saucal_Test_Activator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-saucal-test-deactivator.php
- */
-function deactivate_saucal_test() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-saucal-test-deactivator.php';
-	Saucal_Test_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_saucal_test' );
-register_deactivation_hook( __FILE__, 'deactivate_saucal_test' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-saucal-test.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_saucal_test() {
-
-	$plugin = new Saucal_Test();
-	$plugin->run();
-
-}
-run_saucal_test();
-
-// Add the admin menu item
 function omdb_api_add_admin_menu() {
     add_menu_page(
-        'Saucal Test Settings',       // Page title
-        'OMDB API',                // Menu title
-        'manage_options',             // Capability
-        'omdb-api-settings',       // Menu slug
-        'omdb_api_settings_page',  // Callback function
-        'dashicons-format-video',     // Icon URL
-        20                            // Position (20 ensures it is among the top level)
+        'Saucal Test Settings', 
+        'OMDB API',          
+        'manage_options',           
+        'omdb-api-settings',       
+        'omdb_api_settings_page',  
+        'dashicons-format-video',     
+        20                        
     );
 }
 add_action( 'admin_menu', 'omdb_api_add_admin_menu' );
 
-// Register settings
+/**
+ * Register OMDB API settings.
+ */
 function omdb_api_register_settings() {
     register_setting( 'omdb_api_settings_group', 'omdb_api_key' );
     register_setting( 'omdb_api_settings_group', 'omdb_api_base_url' );
@@ -125,22 +66,29 @@ function omdb_api_register_settings() {
 }
 add_action( 'admin_init', 'omdb_api_register_settings' );
 
-// Callback functions for settings fields
+/**
+ * Callback function for API Key field.
+ */
 function omdb_api_api_key_field_cb() {
     $api_key = get_option( 'omdb_api_key' );
-    echo '<input type="password" name="omdb_api_key" value="' . esc_attr( $api_key ) . '" class="api-input">';
+    echo '<input type="password" name="omdb_api_key" value="' . esc_attr( $api_key ) . '" class="regular-text">';
 }
 
+/**
+ * Callback function for API Base URL field.
+ */
 function omdb_api_api_base_url_field_cb() {
     $api_base_url = get_option( 'omdb_api_base_url' );
-    echo '<input type="text" name="omdb_api_base_url" value="' . esc_attr( $api_base_url ) . '" class="api-input">';
+    echo '<input type="text" name="omdb_api_base_url" value="' . esc_attr( $api_base_url ) . '" class="regular-text">';
 }
 
-// Display the settings page
+/**
+ * Display the OMDB API settings page.
+ */
 function omdb_api_settings_page() {
     ?>
     <div class="wrap">
-        <h1>OMDB API</h1>
+        <h1>OMDB API Settings</h1>
         <form method="post" action="options.php">
             <?php
             settings_fields( 'omdb_api_settings_group' );
@@ -152,3 +100,53 @@ function omdb_api_settings_page() {
     <?php
 }
 
+/**
+ * Add a custom tab to the WooCommerce My Account menu.
+ *
+ * @param array $items Existing menu items.
+ * @return array Modified menu items.
+ */
+function custom_my_account_add_tab( $items ) {
+    $items['omdb_tab'] = __('OMDB Tab', 'saucal-test');
+    return $items;
+}
+add_filter('woocommerce_account_menu_items', 'custom_my_account_add_tab');
+
+/**
+ * Add the endpoint for the custom tab.
+ */
+function omdb_tab_add_my_account_endpoint() {
+    add_rewrite_endpoint('omdb_tab', EP_ROOT | EP_PAGES);
+}
+add_action('init', 'omdb_tab_add_my_account_endpoint');
+
+/**
+ * Display the content for the custom tab using a custom template.
+ */
+function omdb_tab_my_account_endpoint_content() {
+    $template = plugin_dir_path(__FILE__) . 'templates/omdb-tab-template.php';
+    
+    if ( file_exists($template) ) {
+        include $template;
+    } else {
+        echo '<p>' . __('Content not found.', 'saucal-test') . '</p>';
+    }
+}
+add_action('woocommerce_account_omdb_tab_endpoint', 'omdb_tab_my_account_endpoint_content');
+
+/**
+ * Ensure that the new endpoint is flushed on plugin activation.
+ */
+function omdb_tab_flush_rewrite_rules() {
+    omdb_tab_add_my_account_endpoint();
+    flush_rewrite_rules();
+}
+register_activation_hook(__FILE__, 'omdb_tab_flush_rewrite_rules');
+
+/**
+ * Ensure that the endpoint is removed on plugin deactivation.
+ */
+function omdb_tab_remove_rewrite_rules() {
+    flush_rewrite_rules();
+}
+register_deactivation_hook(__FILE__, 'omdb_tab_remove_rewrite_rules');
