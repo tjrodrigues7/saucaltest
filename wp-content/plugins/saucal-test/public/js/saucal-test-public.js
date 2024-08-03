894097949317effcde2d45ca85751f21e5b91015
tjrodrigues7 <131903785+tjrodrigues7@users.noreply.github.com>
2024-08-03 17:45:56 +0100

    OMDB search api
    The search api implementation. users will be able to use the search form and get results from the OMDB API calls.

--- wp-content/plugins/saucal-test/public/js/saucal-test-public.js
+++ wp-content/plugins/saucal-test/public/js/saucal-test-public.js
@@ -1,32 +1,82 @@
 (function( $ ) {
 	'use strict';
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+const baseUrl = `${window.location.protocol}//${window.location.host}`;
+const omdbSearchEndpoint = '/wp-json/omdb/v1/omdb-search';
+const omdbSearchUrl = baseUrl + omdbSearchEndpoint;
+
+let headers = {};
+if (typeof wpApiSettings !== 'undefined') {
+    headers['X-WP-Nonce'] = wpApiSettings.nonce;
+}
+
+/**
+ * Search meadia on OMDB using RestAPI
+ */
+$(document).on('submit', '#omdb-search-form', function(e) {
+	e.preventDefault(); 
+
+	$("#omdb-search-btn").prop('disabled', true).toggle();
+	$("#omdb-search-form .ajax-loader").toggle();
+
+	const formData = new FormData(e.target);
+    const data = {};
+    formData.forEach((value, key) => { data[key] = value; });
+
+	fetch(omdbSearchUrl, {
+		method: 'POST',
+		body: formData,
+		credentials: 'include',
+		headers: headers
+	})
+	.then(response => response.json())
+	.then(data => {
+		if (data.status === 200) {
+			const fields = [
+                { label: 'Title', key: 'Title' },
+                { label: 'Year', key: 'Year' },
+                { label: 'Rated', key: 'Rated' },
+                { label: 'Released', key: 'Released' },
+                { label: 'Runtime', key: 'Runtime' },
+                { label: 'Genre', key: 'Genre' },
+                { label: 'Director', key: 'Director' },
+                { label: 'Writer', key: 'Writer' },
+                { label: 'Actors', key: 'Actors' },
+                { label: 'Plot', key: 'Plot' },
+                { label: 'Language', key: 'Language' },
+                { label: 'Country', key: 'Country' },
+                { label: 'Awards', key: 'Awards' },
+                { label: 'IMDB Rating', key: 'imdbRating' },
+                { label: 'Type', key: 'Type' },
+                { label: 'Box Office', key: 'BoxOffice' },
+            ];
+			
+			let resultsHtml = `<p><strong>Source:</strong> ${data.cached ? 'Cached Results' : 'API Results'}</p>`;
+            resultsHtml += `<img class="omdb-poster" src="${data.results.Poster}" alt="${data.results.Title} Poster" />`;
+
+            fields.forEach(field => {
+                if (data.results[field.key]) {
+                    resultsHtml += `
+                        <span><strong>${field.label}:</strong> ${data.results[field.key]}</span>
+                    `;
+                }
+            });
+
+            $("#omdb-results").html(resultsHtml);
+			
+		} else {
+
+			$("#omdb-results").html("No results found");
+			
+		}
+		$("#omdb-search-form .ajax-loader").toggle();
+		$("#omdb-search-btn").prop('disabled', false).toggle();
+	})
+	.catch((error) => {
+		console.error('Error:', error);
+		$("#omdb-search-btn").prop('disabled', false).toggle();
+		$("#omdb-search-form .ajax-loader").toggle();
+	});
+});
 
 })( jQuery );
